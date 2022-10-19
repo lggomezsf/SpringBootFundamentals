b405f69e9efdab56dfc30d112568566ad429cb34
whynot <anil@panix.com>
2022-10-19 10:09:34 -0400

    Controllers 2

--- code/sbdemo/src/main/java/ttl/larku/controller/StudentController.java
+++ code/sbdemo/src/main/java/ttl/larku/controller/StudentController.java
@@ -2,6 +2,10 @@ package ttl.larku.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ttl.larku.domain.Student;
 import ttl.larku.service.StudentService;
@@ -13,19 +17,36 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
-    @GetMapping("/hello")
-    public String sayit() {
-        return "Hello from here";
-    }
+//    @GetMapping("/hello")
+//    public String sayit() {
+//        return "Hello from here";
+//    }
 
-    @GetMapping("/students")
+    @GetMapping
     public List<Student> getStudents() {
         List<Student> students =  studentService.getAllStudents();
         return students;
     }
+
+    @GetMapping("{id}")
+    public Student getStudent(@PathVariable("id") int id) {
+        Student s = studentService.getStudent(id);
+
+        return s;
+    }
+
+
+    @PostMapping
+    public Student insertStudent(@RequestBody Student student) {
+        Student newStudent = studentService.createStudent(student);
+
+        return newStudent;
+    }
+
 }
